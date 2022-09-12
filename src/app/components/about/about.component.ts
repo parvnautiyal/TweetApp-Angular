0216af4c90fd36de5ef739516eb499fe8f0606f1
Parv Nautiyal <parvnautiyal4@gmail.com>
2022-09-12 17:46:50 +0530

    changes

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {environment} from "../../../environments/environment.prod";
 
 @Component({
   selector: 'app-about',
@@ -6,6 +7,8 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent implements OnInit {
+
+  url = environment.swagger;
 
   constructor() {
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://tweetapp-rest.azurewebsites.net/api/v1.0/tweets/',
-  swagger: 'https://tweetapp-rest.azurewebsites.net/swagger-ui/'
+  baseUrl: 'http://20.253.72.139:80/api/v1.0/tweets/',
+  swagger: 'http://20.253.72.139:80/swagger-ui/'
 };
